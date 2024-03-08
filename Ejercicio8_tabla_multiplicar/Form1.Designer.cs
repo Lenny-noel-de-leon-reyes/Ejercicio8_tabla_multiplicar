316d0f7ede3fc35a0878df138c115e2405b850d9
pc <pc@LENNY>
2024-03-08 10:50:15 -0400

    Agregar archivos de proyecto.

---
+++ Ejercicio8_tabla_multiplicar/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Ejercicio8_tabla_multiplicar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_numero = new System.Windows.Forms.Label();
+            this.txt_numero = new System.Windows.Forms.TextBox();
+            this.lst_tabla = new System.Windows.Forms.ListBox();
+            this.btn_calcular = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_numero
+            // 
+            this.lbl_numero.AutoSize = true;
+            this.lbl_numero.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_numero.Location = new System.Drawing.Point(84, 62);
+            this.lbl_numero.Name = "lbl_numero";
+            this.lbl_numero.Size = new System.Drawing.Size(73, 21);
+            this.lbl_numero.TabIndex = 0;
+            this.lbl_numero.Text = "Numero";
+            // 
+            // txt_numero
+            // 
+            this.txt_numero.Location = new System.Drawing.Point(163, 62);
+            this.txt_numero.Name = "txt_numero";
+            this.txt_numero.Size = new System.Drawing.Size(100, 20);
+            this.txt_numero.TabIndex = 1;
+            // 
+            // lst_tabla
+            // 
+            this.lst_tabla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.lst_tabla.FormattingEnabled = true;
+            this.lst_tabla.Location = new System.Drawing.Point(103, 88);
+            this.lst_tabla.Name = "lst_tabla";
+            this.lst_tabla.Size = new System.Drawing.Size(178, 160);
+            this.lst_tabla.TabIndex = 2;
+            // 
+            // btn_calcular
+            // 
+            this.btn_calcular.BackColor = System.Drawing.Color.Lime;
+            this.btn_calcular.Location = new System.Drawing.Point(67, 253);
+            this.btn_calcular.Name = "btn_calcular";
+            this.btn_calcular.Size = new System.Drawing.Size(75, 23);
+            this.btn_calcular.TabIndex = 3;
+            this.btn_calcular.Text = "Calcular";
+            this.btn_calcular.UseVisualStyleBackColor = false;
+            this.btn_calcular.Click += new System.EventHandler(this.btn_calcular_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.BackColor = System.Drawing.Color.Yellow;
+            this.btn_limpiar.Location = new System.Drawing.Point(148, 253);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 4;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = false;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.Red;
+            this.btn_cerrar.Location = new System.Drawing.Point(229, 253);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(75, 23);
+            this.btn_cerrar.TabIndex = 5;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(359, 308);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_calcular);
+            this.Controls.Add(this.lst_tabla);
+            this.Controls.Add(this.txt_numero);
+            this.Controls.Add(this.lbl_numero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_numero;
+        private System.Windows.Forms.TextBox txt_numero;
+        private System.Windows.Forms.ListBox lst_tabla;
+        private System.Windows.Forms.Button btn_calcular;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_cerrar;
+    }
+}
+
---
+++ Ejercicio8_tabla_multiplicar/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio8_tabla_multiplicar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+         
+            
+                int Numero;
+                Numero = int.Parse(txt_numero.Text);
+
+                for (int i = 1; i <= 12; i++)
+                {
+                    int resultado = Numero * i;
+                    // Agregar la multiplicación al ListBox
+                    lst_tabla.Items.Add($"{Numero} x {i} = {resultado}");
+
+
+
+                }
+            }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            lst_tabla.Items.Clear();
+            txt_numero.Clear();
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            Close();
+
+        }
+    }
+    }
